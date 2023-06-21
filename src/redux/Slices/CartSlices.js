b0379b4e9b70bc62df655057d402341188dd9168
Md Arman <123488184+Armansonu007@users.noreply.github.com>
2023-06-21 19:34:25 +0530

    Initial commit

---
+++ src/redux/Slices/CartSlices.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const Cartslice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    add: () => {},
+    remove: () => {}
+  }
+});
+export const { add, remove } = Cartslice.action;
+export default Cartslice.reducers;
